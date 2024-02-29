791821fd786e56b39e6dfdbd2a983361587b97cf
carlosberserker01 <carlosberserker01@gmail.com>
2024-02-29 09:23:30 -0600

    Final version

--- src/todos/app.js
+++ src/todos/app.js
@@ -1,12 +1,13 @@
 import todoStore, { Filters } from '../store/todo.store';
 import html from './app.html?raw';
-import { renderTodos } from './use-cases';
+import { renderTodos, renderPending } from './use-cases';
 
 const ElementIDs = {
   TodoList: '.todo-list',
   NewTodoInput: '#new-todo-input',
   ClearCompletedButton: '.clear-completed',
-  TodoFilters: '.filtro'
+  TodoFilters: '.filtro',
+  PendingCountLabel: '#pending-count',
 }
 
 /**
@@ -18,10 +19,11 @@ export const App = ( elementId ) => {
   const displayTodos = () => {
     const todos = todoStore.getTodos(todoStore.getCurrentFilter());
     renderTodos( ElementIDs.TodoList, todos );
+    updatePendingCount();
   }
 
   const updatePendingCount = () => {
-    
+    renderPending( ElementIDs.PendingCountLabel )
   }
 
   //Cuando la funcion App() se llama
